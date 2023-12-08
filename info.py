1101b589bab43c7ee50e569133597c0543314875
Filterbot-only <147235633+Filterbot-only@users.noreply.github.com>
2023-12-08 21:31:01 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '29843112'))
 API_HASH = environ.get('API_HASH', '7519cd035add179edec5c84933ab6622')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6694528458:AAExqT-1AJw56jfstHnQTac-3DebvyukNZI")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6694528458:AAGNXiCJ2NBlEvqcfdvkTjky5PEFJtcmOu8")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
